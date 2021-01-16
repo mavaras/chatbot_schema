20c5a283ea83351d405c6c0a5b7eb54d521ea991
Mario Varas <elasgard@hotmail.es>
2021-01-16 12:26:11 -0500

    test

--- constants.py
+++ constants.py
@@ -2,6 +2,6 @@ import os
 
 
 MODEL_PATH = 'model/training_model.h5'
-FB_TOKEN = os.environ['FB_TOKEN']
-FB_WEBHOOKS_VERIFICATION_TOKEN = os.environ['FB_WEBHOOKS_VERIFICATION_TOKEN']
+FB_TOKEN = ''#os.environ['FB_TOKEN']
+FB_WEBHOOKS_VERIFICATION_TOKEN = ''#os.environ['FB_WEBHOOKS_VERIFICATION_TOKEN']
 FB_API_URL = f'https://graph.facebook.com/v5.0/me/messages?access_token={FB_TOKEN}'
